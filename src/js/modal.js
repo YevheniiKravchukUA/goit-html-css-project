a1fc642cdef932ffb8ea2e0e1a56d688052b9fcf
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2022-11-30 12:14:19 +0100

    add scripts to open/close for all modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,6 +13,35 @@
   }
 })();
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-info-open]"),
+    closeModalBtn: document.querySelector("[data-modal-info-close]"),
+    modal: document.querySelector("[data-modal-info]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-down-open]"),
+    closeModalBtn: document.querySelector("[data-modal-down-close]"),
+    modal: document.querySelector("[data-modal-down]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
 
 // data-modal-open  --  на кнопку открытия модалки --/-- добавляем как атрибут
 // data-modal-close  --  на кнопку закрытия модалки --/-- добавляем как атрибут
