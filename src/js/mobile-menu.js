a2faf6b55b8bce35514a07ceb73d35d40f96c61c
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2022-12-02 11:14:07 +0100

    gallery section almost done, add up button, fix uncore links for header and footer, fix scroll speed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLinks = document.querySelectorAll('.mobile__link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +18,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  menuLinks.forEach(menuLink => menuLink.addEventListener('click', toggleMenu));
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
